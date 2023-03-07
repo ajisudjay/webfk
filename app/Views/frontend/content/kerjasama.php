f92ebb55d7ef0aea2a6584411e43932f852cbd9a
Riezfian Raditya Susanto <riezfian@Riezfians-MacBook-Pro.local>
2023-03-07 14:14:12 +0800

    Rapikan Folder frontend

---
+++ app/Views/frontend/content/kerjasama.php
@@ -0,0 +1,58 @@
+<!-- Kerjasama Fakultas -->
+<div class="section_title text-center mb-55 mt-5">
+    <h3>Mitra Kerjasama</h3>
+</div>
+<div class="testmonial_area">
+    <div class="testmonial_active owl-carousel">
+        <div class="single-testmonial testmonial_bg_1 overlay2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-10 offset-xl-1">
+                        <div class="testmonial_info text-center">
+                            <p>Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit
+                            </p>
+                            <div class="testmonial_author">
+                                <h4>Rumah Sakit IA Moeis</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-testmonial testmonial_bg_2 overlay2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-10 offset-xl-1">
+                        <div class="testmonial_info text-center">
+                            <p>Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit
+                            </p>
+                            <div class="testmonial_author">
+                                <h4>Rumah Sakit IA Moeis</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-testmonial testmonial_bg_1 overlay2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-10 offset-xl-1">
+                        <div class="testmonial_info text-center">
+                            <p>Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit Judul Kerjasama Fakultas kedokteran dengan Rumah Sakit
+                            </p>
+                            <div class="testmonial_author">
+                                <h4>Rumah Sakit IA Moeis</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Lihat Semua Kerjasama -->
+    <center class="mt-5">
+        <a href="<?= base_url('/mitra'); ?>" class="boxed-btn3">Lihat Semua</a>
+    </center>
+</div>
+<!-- Akhir Kerjasama Fakultas-->
---
+++ app/Views/frontend/content/slider.php
@@ -0,0 +1,48 @@
+ <!-- Slider Beranda -->
+ <div class="slider_area">
+     <div class="slider_active owl-carousel carousels">
+         <div class="single_slider  d-flex align-items-center slider_bg_2">
+             <div class="container">
+                 <div class="row">
+                     <div class="col-xl-12">
+                         <div class="slider_text ">
+                             <h3> <span>Fakultas <br> Kedokteran</span> <br>
+                                 Universitas <br> Mulawarman </h3>
+                             <br>
+                             <a href="<?= base_url('/informasi'); ?>" class="boxed-btn3">Berita dan Artikel Terbaru</a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="single_slider  d-flex align-items-center slider_bg_1">
+             <div class="container">
+                 <div class="row">
+                     <div class="col-xl-12">
+                         <div class="slider_text ">
+                             <h3> <span>Fakultas <br> Kedokteran</span> <br>
+                                 Universitas <br> Mulawarman </h3>
+                             <br>
+                             <a href="<?= base_url('/informasi'); ?>" class="boxed-btn3">Berita dan Artikel Terbaru</a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="single_slider  d-flex align-items-center slider_bg_2">
+             <div class="container">
+                 <div class="row">
+                     <div class="col-xl-12">
+                         <div class="slider_text ">
+                             <h3> <span>Fakultas <br> Kedokteran</span> <br>
+                                 Universitas <br> Mulawarman </h3>
+                             <br>
+                             <a href="<?= base_url('/informasi'); ?>" class="boxed-btn3">Berita dan Artikel Terbaru</a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- akhir slider beranda -->
