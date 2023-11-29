d408bc6afe2bfcba5691265864dec90e6185a528
Aji Sudjai <ajisudjay@gmail.com>
2023-11-30 01:01:55 +0800

    mengatasi error js

--- app/Views/frontend/layouts/home-js.php
+++ app/Views/frontend/layouts/home-js.php
@@ -1,51 +1,9 @@
 <script>
     const box = document.querySelector(".box");
 
-    const boxmisi = document.querySelector(".box-misi");
-    const boxvisi = document.querySelector(".box-visi");
     const boxberita = document.querySelector(".box-berita");
     const slideInText = document.querySelector(".slide-in-text");
     const slideInText2 = document.querySelector(".slide-in-text-2");
-
-    function checkIfBoxIsInView() {
-        const windowHeight = window.innerHeight;
-        const boxTop = box.getBoundingClientRect().top;
-        const boxBottom = box.getBoundingClientRect().bottom;
-
-        if (boxTop <= windowHeight && boxBottom >= 0) {
-            box.classList.add("active");
-            setTimeout(function() {
-                box.classList.add("show");
-                boxvisi.classList.add("show");
-            }, 500);
-
-            setTimeout(function() {
-                slideInText.classList.add("show");
-            }, 1000);
-        }
-    }
-
-    window.addEventListener("scroll", checkIfBoxIsInView);
-
-    window.addEventListener("scroll", checkIfBoxIsInView2);
-
-    function checkIfBoxIsInView2() {
-        const windowHeight = window.innerHeight;
-        const boxTop = misi.getBoundingClientRect().top;
-        const boxBottom = misi.getBoundingClientRect().bottom;
-
-        if (boxTop <= windowHeight && boxBottom >= 0) {
-            misi.classList.add("active");
-            setTimeout(function() {
-                misi.classList.add("show");
-                boxmisi.classList.add("show");
-            }, 500);
-
-            setTimeout(function() {
-                slideInText2.classList.add("show");
-            }, 1000);
-        }
-    }
 
     const misi = document.querySelectorAll(".box-misi");
 
@@ -60,12 +18,6 @@
             }, index * 500);
         }
     }
-
-    misi.forEach((card, index) => {
-        window.addEventListener("scroll", function() {
-            checkIfCardIsInView(card, index);
-        });
-    });
 
     const cards = document.querySelectorAll(".box-berita");
 
